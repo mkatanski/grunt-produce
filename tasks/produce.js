b61f97e76db566c60760e4a74165e45fa89232e7
Michal Katanski <mike.katanski@gmail.com>
2015-01-15 23:41:36 +0100

    fix(app): Remove displaying possible variables per target
    This functionality is unsupported with new YAML templates definitions

--- tasks/produce.js
+++ tasks/produce.js
@@ -11,31 +11,23 @@ var ProduceModule = require('../lib/ProduceModule.js');
 module.exports = function (grunt) {
     'use strict';
 
-    var MODULE_NAME = 'produce';
-    var MODULE_DESC = 'Automating the process of creating project files';
+    var MODULE_NAME = 'produce',
+        MODULE_DESC = 'Automating the process of creating project files';
 
     grunt.registerMultiTask(MODULE_NAME, MODULE_DESC, function () {
 
         // Merge task-specific and/or target-specific template with these defaults.
         var _options = this.options({
             'templateFile' : ''
-        });
-        // Create new ProduceModule instance
-        var produce = new ProduceModule(grunt);
+            }),
+            // Create new ProduceModule instance
+            produce = new ProduceModule(grunt);
 
         // Prevent running multiple targets
         if (grunt.cli.tasks.toString() === MODULE_NAME) {
             grunt.log.writeln(
-                'You have to specify your target. Possible locals for ' +
-                    grunt.task.current.target + ' are:'
-            );
+                'You have to specify your target. Possible locals for ');
 
-            for (var variableName in _options.locals) {
-                if (!_options.locals.hasOwnProperty(variableName)) {
-                    continue;
-                }
-                grunt.log.writeln('--' + variableName);
-            }
             // stop grunt task
             return true;
         }
@@ -43,29 +35,18 @@ module.exports = function (grunt) {
         try {
             // setup ProduceModule
             produce.setup(_options);
+
+            // If prompting for locals is required
+            if(produce.promptUser) {
+                produce.promptUserAsync(this.async(), function(){
+                    produce.saveFile();
+                });
+            } else {
+                produce.saveFile();
+            }
         }
         catch(err) {
             grunt.fail.fatal(err);
         }
-
-        // If prompting for locals is required
-        if(produce.promptUser) {
-            produce.promptUserAsync(this.async(), function(){
-                try {
-                    produce.saveFile();
-                }
-                catch(err) {
-                    grunt.fail.fatal(err);
-                }
-            });
-        } else {
-            try {
-                produce.saveFile();
-            }
-            catch(err) {
-                grunt.fail.fatal(err);
-            }
-        }
-
     });
 };
